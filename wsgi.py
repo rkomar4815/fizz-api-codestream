6e6ef8e7f6b1b4ec71e83673d82ec0acf9f0495f
Jake Neal <jneal@newrelic.com>
2021-11-04 10:05:31 -0700

    Port repo

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import newrelic.agent
+newrelic.agent.initialize('/home/ec2-user/flask_api_example/newrelic.ini')
+
+import os
+
+from app import create_app
+
+app = create_app(os.getenv("FLASK_ENV") or "test")
+if __name__ == "__main__":
+    app.run(debug=False)
